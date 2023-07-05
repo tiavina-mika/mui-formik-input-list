0f52bf95edc44402e9321ff162b5445f2df7408e
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-07-05 18:35:55 +0300

    remove unused import

--- src/components/FormikAutocompleteField.tsx
+++ src/components/FormikAutocompleteField.tsx
@@ -1,6 +1,5 @@
 import React, { useState, ChangeEvent } from "react";
-import AddIcon from "@mui/icons-material/Add";
-remove unused importremove unused import
+
 import { Field } from "formik";
 import {
   Autocomplete,
